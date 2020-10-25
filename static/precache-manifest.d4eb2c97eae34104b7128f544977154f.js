a30e566e321484867dcbf0459922aa8e4494a9bd
stinkymonkeyph <monkeydev.team@gmail.com>
2020-10-25 20:01:27 +0800

    add ignored files

---
+++ static/precache-manifest.d4eb2c97eae34104b7128f544977154f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d7d52eab6b2dcbefae15a0f8ba40c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c9140079db64f664f7e",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5e813251c08ab48c140f",
+    "url": "/static/css/main.42b773aa.chunk.css"
+  },
+  {
+    "revision": "8c9140079db64f664f7e",
+    "url": "/static/js/2.d4e3def9.chunk.js"
+  },
+  {
+    "revision": "c601096aee8324528068babe49d66278",
+    "url": "/static/js/2.d4e3def9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e813251c08ab48c140f",
+    "url": "/static/js/main.2af3f409.chunk.js"
+  },
+  {
+    "revision": "9be00fb6a0a465ff2eab",
+    "url": "/static/js/runtime-main.353e2e02.js"
+  },
+  {
+    "revision": "52382539737f4e9913e4bf6b9966bee3",
+    "url": "/static/media/Segoe-Bold.52382539.woff"
+  }
+]);
---
+++ static/precache-manifest.e5577579508afbbcdad6fe2d2b420b9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f26245861c2f1f08351a6ad8cde7db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd0cd1bdad79f7c27071",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "238bda551454c17ea792",
+    "url": "/static/css/main.42b773aa.chunk.css"
+  },
+  {
+    "revision": "dd0cd1bdad79f7c27071",
+    "url": "/static/js/2.0d2de6fd.chunk.js"
+  },
+  {
+    "revision": "c7f50805d382e905ca8962e58917bd76",
+    "url": "/static/js/2.0d2de6fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "238bda551454c17ea792",
+    "url": "/static/js/main.70a57138.chunk.js"
+  },
+  {
+    "revision": "9be00fb6a0a465ff2eab",
+    "url": "/static/js/runtime-main.353e2e02.js"
+  },
+  {
+    "revision": "52382539737f4e9913e4bf6b9966bee3",
+    "url": "/static/media/Segoe-Bold.52382539.woff"
+  }
+]);
